accabc317a663a43976bf0b4747b485b2f6fb866
bansi <bansidobariya2510@gmail.com>
2025-05-02 12:33:26 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
-import Pagenotfound from "./pages/Pagenotfound";
+import Pagenotfound from "./pages/PageNotfound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
@@ -32,7 +32,7 @@ function App() {
                   </Protected>
                 }
               />
-              <Route path="*" element={<Pagenotfound />} />
+
               <Route path="app/*" element={<AppLayout />}>
                 <Route index element={<Navigate replace to="cities" />} />
                 <Route path="cities" element={<CityList />} />
